57a226276f93ec64136bd4b6162294c545c0a37e
vickie25 <nyandorovictor3900@gmail.com>
2024-03-18 09:37:21 +0300

    fix: resolve balance drift on transaction delete

--- actions/transaction.js
+++ actions/transaction.js
@@ -314,3 +314,4 @@ function calculateNextRecurringDate(startDate, interval) {
 # updated 2024-03-04 14:11:36
 # updated 2024-03-06 10:58:47
 # updated 2024-03-13 11:02:53
+# updated 2024-03-18 09:37:21
