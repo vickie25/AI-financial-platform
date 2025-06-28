fbd64fa3933e6388b72ef5053ea987ec1c8fbe45
vickie25 <nyandorovictor3900@gmail.com>
2025-06-28 11:30:00 +0300

    fix: transfer creates duplicate entry on destination account

--- actions/transaction.js
+++ actions/transaction.js
@@ -331,3 +331,4 @@ function calculateNextRecurringDate(startDate, interval) {
 # updated 2025-06-22 09:00:00
 # updated 2025-06-25 13:30:00
 # updated 2025-06-28 09:00:00
+# updated 2025-06-28 11:30:00
